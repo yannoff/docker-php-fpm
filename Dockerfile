yannoff/docker-php-fpm

commit 0581b7070c1221e620e72755d3861bef7a815160

[build] Freeze alpine version to 3.13

7.3/Dockerfile
@@ -5,9 +5,9 @@
 # @author  Yannoff <https://github.com/yannoff>
 # @license MIT
 #
-ARG ALPINE_VERSION
+ARG ALPINE_VERSION=3.13
 
-FROM php:7.3-fpm-alpine${ALPINE_VERSION}
+FROM php:fpm-alpine${ALPINE_VERSION}
 
 ARG PHP_EXTS="pdo_mysql pdo_pgsql intl opcache bcmath"
 ARG APK_ADD

7.4/Dockerfile
@@ -5,9 +5,9 @@
 # @author  Yannoff <https://github.com/yannoff>
 # @license MIT
 #
-ARG ALPINE_VERSION
+ARG ALPINE_VERSION=3.13
 
-FROM php:7.4-fpm-alpine${ALPINE_VERSION}
+FROM php:fpm-alpine${ALPINE_VERSION}
 
 ARG PHP_EXTS="pdo_mysql pdo_pgsql intl opcache bcmath"
 ARG APK_ADD

8.0/Dockerfile
@@ -5,9 +5,9 @@
 # @author  Yannoff <https://github.com/yannoff>
 # @license MIT
 #
-ARG ALPINE_VERSION
+ARG ALPINE_VERSION=3.13
 
-FROM php:8.0-fpm-alpine${ALPINE_VERSION}
+FROM php:fpm-alpine${ALPINE_VERSION}
 
 ARG PHP_EXTS="pdo_mysql pdo_pgsql intl opcache bcmath"
 ARG APK_ADD

update.sh
@@ -18,11 +18,11 @@ generate_dockerfile(){
     fi
 
     case ${version} in
-        latest)
-            image="fpm-alpine"
-            ;;
-        [0-9]*)
+        5.5|5.6|7.0|7.1|7.2)
             image="${1}-fpm-alpine"
+            ;;
+        *)
+            image="fpm-alpine\${ALPINE_VERSION}"
             ;;
     esac
     dockerfile=./${version}/Dockerfile
@@ -34,9 +34,9 @@ generate_dockerfile(){
 # @author  Yannoff <https://github.com/yannoff>
 # @license MIT
 #
-ARG ALPINE_VERSION
+ARG ALPINE_VERSION=3.13
 
-FROM php:${image}\${ALPINE_VERSION}
+FROM php:${image}
 
 ARG PHP_EXTS="pdo_mysql pdo_pgsql intl opcache bcmath"
 ARG APK_ADD

5.5/Dockerfile
@@ -5,9 +5,9 @@
 # @author  Yannoff <https://github.com/yannoff>
 # @license MIT
 #
-ARG ALPINE_VERSION
+ARG ALPINE_VERSION=3.13
 
-FROM php:5.5-fpm-alpine${ALPINE_VERSION}
+FROM php:5.5-fpm-alpine
 
 ARG PHP_EXTS="pdo_mysql pdo_pgsql intl opcache bcmath"
 ARG APK_ADD

5.6/Dockerfile
@@ -5,9 +5,9 @@
 # @author  Yannoff <https://github.com/yannoff>
 # @license MIT
 #
-ARG ALPINE_VERSION
+ARG ALPINE_VERSION=3.13
 
-FROM php:5.6-fpm-alpine${ALPINE_VERSION}
+FROM php:5.6-fpm-alpine
 
 ARG PHP_EXTS="pdo_mysql pdo_pgsql intl opcache bcmath"
 ARG APK_ADD

7.0/Dockerfile
@@ -5,9 +5,9 @@
 # @author  Yannoff <https://github.com/yannoff>
 # @license MIT
 #
-ARG ALPINE_VERSION
+ARG ALPINE_VERSION=3.13
 
-FROM php:7.0-fpm-alpine${ALPINE_VERSION}
+FROM php:7.0-fpm-alpine
 
 ARG PHP_EXTS="pdo_mysql pdo_pgsql intl opcache bcmath"
 ARG APK_ADD

7.1/Dockerfile
@@ -5,9 +5,9 @@
 # @author  Yannoff <https://github.com/yannoff>
 # @license MIT
 #
-ARG ALPINE_VERSION
+ARG ALPINE_VERSION=3.13
 
-FROM php:7.1-fpm-alpine${ALPINE_VERSION}
+FROM php:7.1-fpm-alpine
 
 ARG PHP_EXTS="pdo_mysql pdo_pgsql intl opcache bcmath"
 ARG APK_ADD

commit 59558d7f54bb7aacaaddc2986d0b45603fd8b30e

[builder] Fix wrong base php image name
- Fix incorrect base image for versions `7.3`, `7.4`, `8.0`
- Regression introduced in 0581b7070c1221e620e72755d3861bef7a815160

7.3/Dockerfile
@@ -7,7 +7,7 @@
 #
 ARG ALPINE_VERSION=3.13
 
-FROM php:fpm-alpine${ALPINE_VERSION}
+FROM php:7.3-fpm-alpine${ALPINE_VERSION}
 
 ARG PHP_EXTS="pdo_mysql pdo_pgsql intl opcache bcmath"
 ARG APK_ADD

7.4/Dockerfile
@@ -7,7 +7,7 @@
 #
 ARG ALPINE_VERSION=3.13
 
-FROM php:fpm-alpine${ALPINE_VERSION}
+FROM php:7.4-fpm-alpine${ALPINE_VERSION}
 
 ARG PHP_EXTS="pdo_mysql pdo_pgsql intl opcache bcmath"
 ARG APK_ADD

8.0/Dockerfile
@@ -7,7 +7,7 @@
 #
 ARG ALPINE_VERSION=3.13
 
-FROM php:fpm-alpine${ALPINE_VERSION}
+FROM php:8.0-fpm-alpine${ALPINE_VERSION}
 
 ARG PHP_EXTS="pdo_mysql pdo_pgsql intl opcache bcmath"
 ARG APK_ADD

Dockerfile
@@ -5,8 +5,9 @@
 # @author  Yannoff <https://github.com/yannoff>
 # @license MIT
 #
+ARG ALPINE_VERSION=3.13
 
-FROM php:8.1-rc-fpm-alpine
+FROM php:8.0-fpm-alpine${ALPINE_VERSION}
 
 ARG PHP_EXTS="pdo_mysql pdo_pgsql intl opcache bcmath"
 ARG APK_ADD

latest
@@ -1,1 +1,1 @@
-8.1-rc
+8.0

update.sh
@@ -19,10 +19,13 @@ generate_dockerfile(){
 
     case ${version} in
         5.5|5.6|7.0|7.1|7.2)
-            image="${1}-fpm-alpine"
+            image="${version}-fpm-alpine"
+            ;;
+        latest)
+            image="fpm-alpine\${ALPINE_VERSION}"
             ;;
         *)
-            image="fpm-alpine\${ALPINE_VERSION}"
+            image="${version}-fpm-alpine\${ALPINE_VERSION}"
             ;;
     esac
     dockerfile=./${version}/Dockerfile