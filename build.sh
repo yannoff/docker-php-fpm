ad9e9ec15563ff586e1b2d29906f71b655f6e8df
Yannoff <yannoff.it@gmail.com>
2021-10-31 11:32:23 +0100

    [build] Improve latest tag handling
    - Fix `latest` tag on-demand build
    - Automatically push alias when appropriate

--- build.sh
+++ build.sh
@@ -8,8 +8,12 @@
 image=yannoff/php-fpm
 logfile=./build.log
 
+get_latest_numver(){
+    ls -l latest | awk '{ print $NF; }'
+}
+
 build_and_push(){
-    local version=$1 tag=$2
+    local version=$1 tag=$2 latest
     [ -z ${tag} ] && tag=${version}-fpm-alpine
     printf "\033[01mProcessing version %s...\033[00m\n" "${version}"
     cd ${version}
@@ -17,18 +21,24 @@ build_and_push(){
     docker pull php:${tag}
     printf "\033[01mBuilding image %s:%s...\033[00m\n" "${image}" "${tag}"
     docker build -t ${image}:${tag} . 2>&1 >>${logfile} && docker push ${image}:${tag}
+    cd -
     printf "\033[01mCreating shortcut image %s:%s...\033[00m\n" "${image}" "${version}"
     docker tag ${image}:${tag} ${image}:${version} && docker push ${image}:${version}
+    latest=$(get_latest_numver)
+    if [ "${version}" == "${latest}" ]
+    then
+        printf "\033[01mCreating shortcut image %s:latest (=> %s)...\033[00m\n" "${image}" "${version}"
+        docker tag ${image}:${version} ${image}:latest && docker push ${image}:latest
+    fi
     printf "\033[01mCleaning assets...\033[00m\n"
     docker rmi ${image}:${version} ${image}:${tag} php:${tag}
-    cd -
     printf "Building image \033[01m%s:%s\033[00m ...\033[01;32mOK\033[00m\n" "${image}" "$tag"
 }
 
 if [ $# -eq 0 ]
 then
     # If no version specified, build and push all versions
-    set -- 5.5 5.6 7.0 7.1 7.2 7.3 7.4 8.0 8.1-rc latest
+    set -- 5.5 5.6 7.0 7.1 7.2 7.3 7.4 8.0 8.1-rc
 fi
 
 printf "\033[01mUpdating %s ...\033[00m\n" "mlocati/php-extension-installer"
@@ -38,5 +48,10 @@ rm ${logfile}
 
 for v in "$@"
 do
-    build_and_push ${v}
+    if [ "${v}" == "latest" ]
+    then
+        build_and_push ${v} latest
+    else
+        build_and_push ${v}
+    fi
 done
